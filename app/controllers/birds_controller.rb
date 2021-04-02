e85aa6b3a158beb02f5520b4fa386dbb2d4acab5
beendra <alexandra.ricardel@gmail.com>
2021-04-02 11:13:06 -0500

    commit tree 1eea5b103a701e802112e9de8e02cb24b46f28d6

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,12 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render json: @birds
+    birds = Bird.all
+    render json: birds.to_json(except: [:created_at, :updated_at])
   end
+
+  def show
+    bird = Bird.find_by(id: params[:id])
+    render json: birds.to_json(except: [:created_at, :updated_at])
+  end
+
 end
